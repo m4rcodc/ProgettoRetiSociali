e31aeb2012f1254d7f9351d5efc3e0c8e8de31d2
AndreaT98 <a.tranquillo1@studenti.unisa.it>
2023-05-20 17:20:31 +0200

    Plot implementation and other fixes

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from random import seed
 from random import random
 from collections import Counter
 import graph
+import matplotlib.pyplot as plt
 
 
 #--------------------------------------------------------------------------------------------------
 #Main function
+avg_values=[]
 
 #Graph import and visualization
 G2 = snap.GenRndGnm(snap.TUNGraph, 40, 70)
@@ -41,8 +43,24 @@ for i in range(4,8):
         print("Length of influenced nodes: ",len(nodes))
         avg_len += len(nodes)
 
+    avg_values.append(avg_len/10)
     #Final print
     print("Main completed")
     print("--------------------------------------------Result--------------------------------------------------")
     print('Average number of influenced nodes: ',avg_len/10)
     print("----------------------------------------------------------------------------------------------------")
+
+
+# Dati di esempio
+k = [4, 5, 6, 7]  # Valori assegnati a "k"
+
+# Creazione del grafico
+plt.plot(k, avg_values, marker='o', linestyle='-', color='b')
+
+# Titoli degli assi e del grafico
+plt.xlabel('k')
+plt.ylabel('avg_value')
+plt.title('Grafico k vs avg_values')
+
+# Visualizzazione del grafico
+plt.show()
